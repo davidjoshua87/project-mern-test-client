9e251be0d1db5358f0644605f2920016e7e2fd75
davidjoshua87 <davidjosh_87@yahoo.com>
2022-09-21 07:36:48 +0700

    fix: #1 edit home page.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,16 +6,19 @@ function Home() {
 		<div className="container text-center home" style={{ marginTop: "60px" }}>
 			<div className="card">
 				<div className="card-header" style={{ marginBottom: "20px" }}>
-					<h1>Data Patients</h1>
+					<h1>Data Patients Management</h1>
 				</div>
 				<p>
-					<b>Front-end</b>: React.js v17+
+					<b>Front-end</b>: React.js
 				</p>
 				<p>
 					<b>Back-end</b>: Node.js, Express.js
 				</p>
 				<p>
 					<b>Database</b>: MongoDB Atlas with Mongoose ODM
+				</p>
+				<p>
+					<b>Deployment</b>: Vercel
 				</p>
 				<p>
 					<b>Developed By</b>: JDs Web Developer
